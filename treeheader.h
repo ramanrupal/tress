b72de8238841de65c6f03c79a44f43840b795560
Ramanpreet Kaur <32161711+ramanrupal@users.noreply.github.com>
2018-10-13 22:48:22 +0530

    Update treeheader.h
    height func added

--- treeheader.h
+++ treeheader.h
@@ -91,6 +91,17 @@ void printLevelorder(struct node *root)
 		q.pop();
 	}
 }
+int height(struct node *root)
+{
+	if(root==NULL)
+		return 0;
+	int lheight=height(root->left);
+	int rheight=height(root->right);
+	if(lheight>rheight)
+		return lheight+1;
+	else
+		return rheight+1;
+}
 void printRevLevelorder(struct node *root)
 {
 	if(root==NULL)
